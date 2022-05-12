d6d97cc0094e1bee0f4ab15216d80aea66316d86
JordawnTheJew <zelda199530@gmail.com>
2022-05-12 06:34:00 -0700

    0.0.5 Snapshot, able to place and delete blocks, including the ground one.

--- CS100B/Project/MineCraft project/MinecraftProject.py
+++ CS100B/Project/MineCraft project/MinecraftProject.py
@@ -1,20 +1,9 @@
+from tkinter import Scale
 from tkinter.messagebox import YES
 from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
 from numpy import floor
 from perlin_noise import PerlinNoise
-
-
-  #keybinds 
-def input(key):
-    
-    if key == "escape":
-        quit()
-    if key == 'left mouse down':
-        place()
-    elif key == 'right mouse down':
-        e = mouse.hovered_entity
-        destroy(e)
 
 
 MC = Ursina()
@@ -28,19 +17,19 @@ window.exit_button = True
 window.color = color.black
 window.make_editor_gui
 scene.fog_density = .005
-scene.fog_color = color.green
+scene.fog_color = color.salmon
 SkyBox = Sky()
 
 #Preload Textures
 grass = load_texture('dirt')
-wood = load_texture('stoneBlock')
+stone = load_texture('stoneBlock')
 
 
 
 
 #Objects
-grass = Entity(model='cube', texture = 'dirt', Collider = True)
-pic = Entity(model='cube', texture= "stoneBlock", Collider = True)
+grass = Entity(model='cube', texture = 'dirt')
+pic = Entity(model='cube', texture= "wiremesh", scale=(1,1,1))
 
 
 
@@ -65,59 +54,67 @@ Builder.height= .5
 FirstPersonController
 
 
-
-
 def pickaxe():
     pic.position = round(Builder.position + camera.forward * 3)
     pic.y += 2
-
     pic.x = round(pic.x)
     pic.y = round(pic.y)
     pic.z = round(pic.z)
-
-    
 #Building
 def place():
-    p = duplicate (pic)
-    p = collider = 'cube'
+    p = duplicate(pic)
+    p.texture = 'stoneBlock' 
+    p.model = 'cube'
+    p.collider = 'cube'
+
+  #keybinds 
+def input( key):
+    
+    if key == "escape":
+        quit()
+    if key == 'left mouse down':
+        place()
+    if key == 'right mouse down':
+        e = mouse.hovered_entity
+        destroy(e)
 
         
 
 #Ground or Floor
 Ground = Entity(model = None, collider = None)
-GroundDepth = PerlinNoise (octaves=3.5,seed=216)
+GroundVariation = PerlinNoise (octaves=3.5,seed=216)
 
 Groundx = 32 #Ground Width
-
+Groundz = 8
 #Ground modifiers
-MaxX = 9
+MaxX = 32
     
      #World height
-Variation = 48
+Variation = 100
 
 for g in range(Groundx*Groundx):
     grass = Entity(model='cube', texture = 'dirt')
     grass.x = floor(g/Groundx)
     grass.z = floor(g%Groundx)
-    grass.y = floor((GroundDepth([grass.x/Variation,grass.z/Variation]))*MaxX) #Ground depth to add immersion, x and z are devided by int in Variation, and multiplied by MaxX
+    grass.y = floor((GroundVariation([grass.x/Variation,grass.z/Variation]))*MaxX) #Ground variation to add immersion, x and z are devided by int in Variation, and multiplied by MaxX
     grass.parent = Ground   #Added the ground as a parent class just in case i want to use the grass block later without messing up the floor
-
+    grass.collider = 'mesh'
 
 
 
 
 #Ground collision
-Ground.combine(auto_destroy = False)
-Ground.collider = 'mesh'
+#Ground.combine(auto_destroy = False) //Caused deletion of ground...
+Ground.collider = None
 Ground.texture = 'dirt'
-
+Ground.model = 'cube'
 #update loop to check builder y position in order to determine if builder can jump on a block
 def update():
     Blockcollision=False
 
     if Blockcollision==True:
         # With Delta time it will always update on the same intervals regardless of users fps
-        Builder.y = lerp(Builder.y, target, 5 * time.dt) 
+        Builder.y = lerp(Builder.y, target_y, 1234* time.dt) 
         #Builder towards ground
     else:
          Blockcollision-= 8 * time.dt
