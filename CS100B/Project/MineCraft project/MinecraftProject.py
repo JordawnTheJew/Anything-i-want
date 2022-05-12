35de1cc4df985176036469371d5417d8fbbf76d9
JordawnTheJew <zelda199530@gmail.com>
2022-05-12 00:57:56 -0700

    Snapshot 0.0.4 able to place blocks but not delete them, deletes the terrain if looking at it.... help me

--- CS100B/Project/MineCraft project/MinecraftProject.py
+++ CS100B/Project/MineCraft project/MinecraftProject.py
@@ -1,9 +1,26 @@
+from tkinter.messagebox import YES
 from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
 from numpy import floor
 from perlin_noise import PerlinNoise
 
+
+  #keybinds 
+def input(key):
+    
+    if key == "escape":
+        quit()
+    if key == 'left mouse down':
+        place()
+    elif key == 'right mouse down':
+        e = mouse.hovered_entity
+        destroy(e)
+
+
 MC = Ursina()
+
+
+
 
 #Window Settings
 #window.fullscreen = True
@@ -11,18 +28,22 @@ window.exit_button = True
 window.color = color.black
 window.make_editor_gui
 scene.fog_density = .005
-scene.fog_color 
+scene.fog_color = color.green
 SkyBox = Sky()
-def input(key):
-    if key == "escape":
-        quit()
-
 
 #Preload Textures
 grass = load_texture('dirt')
+wood = load_texture('stoneBlock')
+
+
+
 
 #Objects
-grass = Entity(model='cube', texture = 'dirt')
+grass = Entity(model='cube', texture = 'dirt', Collider = True)
+pic = Entity(model='cube', texture= "stoneBlock", Collider = True)
+
+
+
 #grass.color = color.green
 
 #FP Char
@@ -31,10 +52,10 @@ Builder.x = 5
 Builder.y = 2
 
 #FP Char modifiers
-Builder.cursor.visible = False
+Builder.cursor.visible = True
 Builder.gravity = .5
 Builder.jump_height = 1
-#Builder.fall_after 
+"Builder.fall_after"
 Builder.jump_up_duration = .9
 Builder.fade_in = True
 Builder.speed = 5
@@ -43,6 +64,25 @@ camera.fov = 110
 Builder.height= .5
 FirstPersonController
 
+
+
+
+def pickaxe():
+    pic.position = round(Builder.position + camera.forward * 3)
+    pic.y += 2
+
+    pic.x = round(pic.x)
+    pic.y = round(pic.y)
+    pic.z = round(pic.z)
+
+    
+#Building
+def place():
+    p = duplicate (pic)
+    p = collider = 'cube'
+
+        
+
 #Ground or Floor
 Ground = Entity(model = None, collider = None)
 GroundDepth = PerlinNoise (octaves=3.5,seed=216)
@@ -50,7 +90,9 @@ GroundDepth = PerlinNoise (octaves=3.5,seed=216)
 Groundx = 32 #Ground Width
 
 #Ground modifiers
-MaxX = 16 #World hight
+MaxX = 9
+    
+     #World height
 Variation = 48
 
 for g in range(Groundx*Groundx):
@@ -60,10 +102,32 @@ for g in range(Groundx*Groundx):
     grass.y = floor((GroundDepth([grass.x/Variation,grass.z/Variation]))*MaxX) #Ground depth to add immersion, x and z are devided by int in Variation, and multiplied by MaxX
     grass.parent = Ground   #Added the ground as a parent class just in case i want to use the grass block later without messing up the floor
 
+
+
+
+
+#Ground collision
 Ground.combine(auto_destroy = False)
 Ground.collider = 'mesh'
 Ground.texture = 'dirt'
 
+#update loop to check builder y position in order to determine if builder can jump on a block
+def update():
+    Blockcollision=False
 
+    if Blockcollision==True:
+        # With Delta time it will always update on the same intervals regardless of users fps
+        Builder.y = lerp(Builder.y, target, 5 * time.dt) 
+        #Builder towards ground
+    else:
+         Blockcollision-= 8 * time.dt
+
+    #update objects
+    pickaxe()
+
+
+        
+        
+            
 
 MC.run()
